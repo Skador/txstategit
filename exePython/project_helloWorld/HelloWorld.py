c9da6c055852d83d49c3cc6f26b8cbf03bc4c789
Cody Kern <cody.kern@mozido.com>
2015-09-15 20:43:24 -0500

    Moving files and adding more items

---
+++ exePython/project_helloWorld/HelloWorld.py
@@ -0,0 +1,21 @@
+__author__ = 'ckern1'
+print "Hello World"
+print ""
+#int num = 5
+num=5
+
+num=73
+sen="Butts."
+print sen+str(num)
+num=str(num)
+print len(num)
+print ""
+
+str_1 = "Chocolate"
+str_2 = "delicious"
+
+print "%s is super %s."%(str_1,str_2)
+print ""
+
+num2 = raw_input("Input a number.")
+print num2
