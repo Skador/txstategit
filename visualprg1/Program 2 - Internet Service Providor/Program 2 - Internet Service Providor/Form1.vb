26e9b4351c97e28a5f5e2c6bf8af54f25569cc74
Cody Kern <cody.kern@mozido.com>
2015-09-17 14:29:49 -0500

    Completed Program

--- visualprg1/Program 2 - Internet Service Providor/Program 2 - Internet Service Providor/Form1.vb
+++ visualprg1/Program 2 - Internet Service Providor/Program 2 - Internet Service Providor/Form1.vb
@@ -8,6 +8,7 @@
         Dim total As Decimal
         Dim subTotal As Decimal
         Dim hours As Decimal
+        Const hrsInMonth As Decimal = 744
         Const rateA As Decimal = 9.95
         Const limitA As Decimal = 10
         Const extraHrsAcost As Decimal = 2
@@ -17,31 +18,38 @@
         Const rateC As Decimal = 19.95
         Const discount As Decimal = 0.2
         Try
-            If rbPackageA.Checked = True Then
-                If CDec(txtNbrHrsEntered.Text) > limitA Then
-                    hours = CDec(txtNbrHrsEntered.Text) - limitA
-                    subTotal = rateA + (hours * extraHrsAcost)
-                Else
-                    subTotal = rateA
-                End If
-            ElseIf rbPackageB.Checked = True Then
-                If CDec(txtNbrHrsEntered.Text) > limitB Then
-                    hours = CDec(txtNbrHrsEntered.Text) - limitB
-                    subTotal = rateB + (hours * extraHrsBcost)
-                Else
-                    subTotal = rateB
-                End If
-            Else
-                subTotal = rateC
+            If CDec(txtNbrHrsEntered.Text) > hrsInMonth Then
+                Error 1
             End If
-            If cbNonProfOrg.Checked = True Then
-                total = subTotal - (subTotal * discount)
-            Else
-                total = subTotal
-            End If
-            lblTotalAmount.Text = total.ToString("C")
+            Try
+                If rbPackageA.Checked = True Then
+                    If CDec(txtNbrHrsEntered.Text) > limitA Then
+                        hours = CDec(txtNbrHrsEntered.Text) - limitA
+                        subTotal = rateA + (hours * extraHrsAcost)
+                    Else
+                        subTotal = rateA
+                    End If
+                ElseIf rbPackageB.Checked = True Then
+                    If CDec(txtNbrHrsEntered.Text) > limitB Then
+                        hours = CDec(txtNbrHrsEntered.Text) - limitB
+                        subTotal = rateB + (hours * extraHrsBcost)
+                    Else
+                        subTotal = rateB
+                    End If
+                Else
+                    subTotal = rateC
+                End If
+                If cbNonProfOrg.Checked = True Then
+                    total = subTotal - (subTotal * discount)
+                Else
+                    total = subTotal
+                End If
+                lblTotalAmount.Text = total.ToString("C")
+            Catch ex As Exception
+                MessageBox.Show("The value must be numeric")
+            End Try
         Catch ex As Exception
-            MessageBox.Show("The value must be numeric")
+            MessageBox.Show("Hours can not exceed 744 hours")
         End Try
     End Sub
 
