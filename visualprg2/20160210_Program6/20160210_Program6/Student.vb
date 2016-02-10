080476179220abb26e0a06f8c89ba61318b4088c
Cody Kern <Cody.kern@mozido.com>
2016-02-10 17:13:00 -0600

    Created Student Class
    Created the student class along with variables and constuctor

---
+++ visualprg2/20160210_Program6/20160210_Program6/Student.vb
@@ -0,0 +1,12 @@
+﻿Public Class Student
+    Public Property studID As Integer
+    Public Property lastName As String
+    Public Property studGPA As Double
+
+    Public Sub New(ByVal pStudID As Integer = 0, ByVal pLastName As String = "", ByVal pStudGPA As Double)
+        studID = pStudID
+        lastName = pLastName
+        studGPA = pStudGPA
+    End Sub
+
+End Class
