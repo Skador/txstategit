b9cc34c3d17e8d2b656d74a72a55568b7f08d123
Cody Kern <Cody.kern@mozido.com>
2016-02-13 13:24:34 -0600

    Added Error Handling

--- visualprg2/Project1-CIS3325/Project1-CIS3325/Form1.vb
+++ visualprg2/Project1-CIS3325/Project1-CIS3325/Form1.vb
@@ -1,10 +1,10 @@
 ﻿Public Class Form1
-    Dim salEmployee As New Employee
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
 
     Private Sub SaveBtn_Click(sender As Object, e As EventArgs) Handles SaveBtn.Click
+        Dim salEmployee As New Employee
         'Once the button is pressed, The values in the textboxes are transfered to the new Employee
         salEmployee.firstName = FirstNameTxt.Text
         salEmployee.lastName = LastNameTxt.Text
@@ -16,5 +16,58 @@
         'Shows the Salary data in a Currency format
         MonthlySalaryLbl.Text = salEmployee.monthlySalary().ToString("c")
         YearlySalaryLbl.Text = salEmployee.yearlySalary().ToString("c")
+    End Sub
+
+    Private Sub NoOfWeekTxt_Leave(sender As Object, e As EventArgs) Handles NoOfWeekTxt.Leave
+        Try
+            If IsNumeric(CDec(NoOfWeekTxt.Text)) = True Then
+                If CDec(NoOfWeekTxt.Text) < 1 Or CDec(NoOfWeekTxt.Text) > 4 Then
+                    MsgBox("Please enter an interger between the values of 1 and 4.")
+                    NoOfWeekTxt.Focus()
+                End If
+            End If
+            
+        Catch ex As Exception
+            MsgBox("Please only enter integers")
+            NoOfWeekTxt.Focus()
+        End Try
+    End Sub
+
+    Private Sub WeeklyHoursTxt_Leave(sender As Object, e As EventArgs) Handles WeeklyHoursTxt.Leave
+          Try
+            If IsNumeric(CDec(WeeklyHoursTxt.Text)) = True Then
+                If CDec(WeeklyHoursTxt.Text) < 1 Or CDec(WeeklyHoursTxt.Text) > 40 Then
+                    MsgBox("Please enter an interger between the values of 1 and 40.")
+                    WeeklyHoursTxt.Focus()
+                End If
+            End If
+        Catch ex As Exception
+            MsgBox("Please only enter integers")
+            WeeklyHoursTxt.Focus()
+        End Try
+    End Sub
+
+    Private Sub SalaryPerHourTxt_Leave(sender As Object, e As EventArgs) Handles SalaryPerHourTxt.Leave
+        Try
+            If IsNumeric(CDec(SalaryPerHourTxt.Text)) = True Then
+            End If
+        Catch ex As Exception
+            MsgBox("Please only enter integers")
+            SalaryPerHourTxt.Focus()
+        End Try
+    End Sub
+
+    Private Sub MonthsOfPaymentTxt_Leave(sender As Object, e As EventArgs) Handles MonthsOfPaymentTxt.Leave
+        Try
+            If IsNumeric(CDec(MonthsOfPaymentTxt.Text)) = True Then
+                If CDec(MonthsOfPaymentTxt.Text) < 1 Or CDec(MonthsOfPaymentTxt.Text) > 12 Then
+                    MsgBox("Please enter an interger between the values of 1 and 12.")
+                    MonthsOfPaymentTxt.Focus()
+                End If
+            End If
+        Catch ex As Exception
+            MsgBox("Please only enter integers")
+            MonthsOfPaymentTxt.Focus()
+        End Try
     End Sub
 End Class
