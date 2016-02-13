0e014e4fc5ed212a257a4b5fb91ccb68f0e7ef02
lawhaley95 <law144@txstate.edu>
2016-02-13 13:16:56 -0600

    Corrected Salary Per Hour Display in Text File
    Corrected how the salary per hour is displayed in the text file so that
    it is displayed in the currency format.

--- visualprg2/Project1-CIS3325/Project1-CIS3325/Form1.vb
+++ visualprg2/Project1-CIS3325/Project1-CIS3325/Form1.vb
@@ -3,6 +3,8 @@ Public Class Form1
     Dim salEmployee As New Employee
 
     Dim writeFile As StreamWriter
+
+    Dim Wage As Integer
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
@@ -20,18 +22,22 @@ Public Class Form1
         MonthlySalaryLbl.Text = salEmployee.monthlySalary().ToString("c")
         YearlySalaryLbl.Text = salEmployee.yearlySalary().ToString("c")
 
+        'Simply assigns a variable called 'Wage' so that salary per hour will appear in the currency format in the text file.
+        Wage = SalaryPerHourTxt.Text
+
         'Imports the data gathered form the user into a text file called 'Employee'
         If File.Exists("../../Employee.txt") Then
             MsgBox("Data will be appended to the 'Employee' record file.")
             writeFile = File.AppendText("../../Employee.txt")
-            writeFile.WriteLine("First Name: " & FirstNameTxt.Text & vbNewLine & "Last Name: " & LastNameTxt.Text & vbNewLine & "Number of Weeks Worked: " & NoOfWeekTxt.Text & vbNewLine & "Weekly Hours: " & WeeklyHoursTxt.Text & vbNewLine & "Salary per Hour: " & SalaryPerHourTxt.Text & vbNewLine & "Monthly Salary: " & MonthlySalaryLbl.Text & vbNewLine & "Months of Payment: " & MonthsOfPaymentTxt.Text & vbNewLine & "Yearly Salary: " & YearlySalaryLbl.Text & vbNewLine)
+            Wage = salEmployee.salarPerHour().ToString("c")
+            writeFile.WriteLine("First Name: " & FirstNameTxt.Text & vbNewLine & "Last Name: " & LastNameTxt.Text & vbNewLine & "Number of Weeks Worked: " & NoOfWeekTxt.Text & vbNewLine & "Weekly Hours: " & WeeklyHoursTxt.Text & vbNewLine & "Salary per Hour: " & Wage.ToString("c") & vbNewLine & "Monthly Salary: " & MonthlySalaryLbl.Text & vbNewLine & "Months of Payment: " & MonthsOfPaymentTxt.Text & vbNewLine & "Yearly Salary: " & YearlySalaryLbl.Text & vbNewLine)
             MsgBox("Data was successfully appended to the 'Employee' record file.")
             writeFile.Close()
         Else
             MsgBox("A record file has not been created. A file named 'Employee' will be created.")
             writeFile = File.CreateText("../../Employee.txt")
             MsgBox("A record has been created for " & FirstNameTxt.Text & " " & LastNameTxt.Text & " in the 'Employee' file.")
-            writeFile.WriteLine("First Name: " & FirstNameTxt.Text & vbNewLine & "Last Name: " & LastNameTxt.Text & vbNewLine & "Number of Weeks Worked: " & NoOfWeekTxt.Text & vbNewLine & "Weekly Hours: " & WeeklyHoursTxt.Text & vbNewLine & "Salary per Hour: " & SalaryPerHourTxt.Text & vbNewLine & "Monthly Salary: " & MonthlySalaryLbl.Text & vbNewLine & "Months of Payment: " & MonthsOfPaymentTxt.Text & vbNewLine & "Yearly Salary: " & YearlySalaryLbl.Text & vbNewLine)
+            writeFile.WriteLine("First Name: " & FirstNameTxt.Text & vbNewLine & "Last Name: " & LastNameTxt.Text & vbNewLine & "Number of Weeks Worked: " & NoOfWeekTxt.Text & vbNewLine & "Weekly Hours: " & WeeklyHoursTxt.Text & vbNewLine & "Salary per Hour: " & Wage.ToString("c") & vbNewLine & "Monthly Salary: " & MonthlySalaryLbl.Text & vbNewLine & "Months of Payment: " & MonthsOfPaymentTxt.Text & vbNewLine & "Yearly Salary: " & YearlySalaryLbl.Text & vbNewLine)
             MsgBox("Employee record was successfully added to file.")
             writeFile.Close()
         End If
