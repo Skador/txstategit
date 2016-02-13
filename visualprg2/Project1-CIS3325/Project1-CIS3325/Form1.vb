5f2e97c2bf2d72d1f4ff57d1e353d21e7e930768
lawhaley95 <law144@txstate.edu>
2016-02-13 12:50:52 -0600

    Created WriteFile Parameters
    Cody,
    
    Wrote the code that writes each employee 'record' to a text file called
    'Employee' inside the project file. You can delete the 'Employee' text
    file if it already exists to see how it runs from scratch when creating
    the new file.
    
    Luke

--- visualprg2/Project1-CIS3325/Project1-CIS3325/Form1.vb
+++ visualprg2/Project1-CIS3325/Project1-CIS3325/Form1.vb
@@ -1,5 +1,8 @@
-﻿Public Class Form1
+﻿Imports System.IO
+Public Class Form1
     Dim salEmployee As New Employee
+
+    Dim writeFile As StreamWriter
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
@@ -16,5 +19,21 @@
         'Shows the Salary data in a Currency format
         MonthlySalaryLbl.Text = salEmployee.monthlySalary().ToString("c")
         YearlySalaryLbl.Text = salEmployee.yearlySalary().ToString("c")
+
+        'Imports the data gathered form the user into a text file called 'Employee'
+        If File.Exists("../../Employee.txt") Then
+            MsgBox("Data will be appended to the 'Employee' record file.")
+            writeFile = File.AppendText("../../Employee.txt")
+            writeFile.WriteLine("First Name: " & FirstNameTxt.Text & vbNewLine & "Last Name: " & LastNameTxt.Text & vbNewLine & "Number of Weeks Worked: " & NoOfWeekTxt.Text & vbNewLine & "Weekly Hours: " & WeeklyHoursTxt.Text & vbNewLine & "Salary per Hour: " & SalaryPerHourTxt.Text & vbNewLine & "Monthly Salary: " & MonthlySalaryLbl.Text & vbNewLine & "Months of Payment: " & MonthsOfPaymentTxt.Text & vbNewLine & "Yearly Salary: " & YearlySalaryLbl.Text & vbNewLine)
+            MsgBox("Data was successfully appended to the 'Employee' record file.")
+            writeFile.Close()
+        Else
+            MsgBox("A record file has not been created. A file named 'Employee' will be created.")
+            writeFile = File.CreateText("../../Employee.txt")
+            MsgBox("A record has been created for " & FirstNameTxt.Text & " " & LastNameTxt.Text & " in the 'Employee' file.")
+            writeFile.WriteLine("First Name: " & FirstNameTxt.Text & vbNewLine & "Last Name: " & LastNameTxt.Text & vbNewLine & "Number of Weeks Worked: " & NoOfWeekTxt.Text & vbNewLine & "Weekly Hours: " & WeeklyHoursTxt.Text & vbNewLine & "Salary per Hour: " & SalaryPerHourTxt.Text & vbNewLine & "Monthly Salary: " & MonthlySalaryLbl.Text & vbNewLine & "Months of Payment: " & MonthsOfPaymentTxt.Text & vbNewLine & "Yearly Salary: " & YearlySalaryLbl.Text & vbNewLine)
+            MsgBox("Employee record was successfully added to file.")
+            writeFile.Close()
+        End If
     End Sub
 End Class
