00be63ec3112a0f203091c530f8853e4dc6a5656
Cody Kern <Cody.kern@mozido.com>
2016-02-22 18:04:31 -0600

    New Project!

---
+++ visualprg2/SQLFILES/20160222_SQLProgram7.sql
@@ -0,0 +1,16 @@
+﻿SELECT * FROM Student
+
+SELECT StudentId, StuFirstName FROM Student
+
+INSERT INTO Student(StuFirstName, StuLastName, DateOfBirth) VALUES ('Mary', 'Johnson', '03/03/2015')
+
+INSERT INTO Student(StuFirstName, StuLastName, DateOfBirth) VALUES ('Mary', 'Smith', '03/03/2015')
+
+UPDATE Student SET StuLastName='Poppins' WHERE StudentID=5
+
+UPDATE Student SET StuLastName='Johnson' WHERE StudentID=4
+
+DELETE FROM Student WHERE StudentID=3
+
+DELETE FROM Student WHERE StudentID=2
+
